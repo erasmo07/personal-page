fda6946ced1301192fd982996ebb12164a04117a
erasmo07 <erasmomontes07@hotmail.com>
2016-10-01 12:00:34 -0400

    Changed: Put template in web app.

--- personal_page/settings.py
+++ personal_page/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'personal_page.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'web', 'templates'),
+                 os.path.join(BASE_DIR, 'blog', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
